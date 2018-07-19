5903fc05a9d928db6410ffd8a3ea230cf8534c5f
Vanderstichel Jean-Baptiste <vjean-baptiste@localhost.localdomain>
2018-07-19 10:36:57 +0200

    main.py added - need to write into a file, but data are stored

---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+from train_data import get_trained_data
+
+# detection of face + use of webcam
+import cv2
+
+# keyboard imput
+import keyboard
+
+# error if the is no name
+if len(sys.argv) != 2 :
+    print("usage :\n python3", sys.argv[0], "\"name\"")
+
+# preparing webcam
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# exit variable
+want_to_quit = 0
+# list of images
+new_data = []
+
+while not want_to_quit :
+    ret, frame = cap.read()
+
+    # getting face position
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, 1.3, 7)
+
+    # printing all squares around faces
+    for (x, y, w, h) in face :
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    
+    # print cam
+    cv2.imshow("cam", frame)
+    cv2.waitKey(1)
+    
+    # add an image into the list
+    if keyboard.is_pressed(' ') and len(face) == 1 :
+        new_data.append(get_trained_data(gray, face))
+        
+    # quit
+    if keyboard.is_pressed('q') :
+        want_to_quit = 1
+
+for image in new_data :
+    for line in image :
+        print(line)
+    print()
+
+
+print("we got", len(new_data), "images :")
--- train_data.py
+++ train_data.py
@@ -1,10 +1,4 @@
-import tensorflow as ts
-from tensorflow import keras
-import numpy as np
 import cv2
-
-cap = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('/home/acohen/Projet_Perso/openCV/haarcascade_xml/haarcascade_frontalface_default.xml')
 
 def frame_formating(frame, face):
     for (x, y, h, w) in face:
@@ -12,12 +6,11 @@ def frame_formating(frame, face):
         crop_img = cv2.resize(crop_img, (28, 28))        
     return crop_img
 
-def get_train_data(frame, face):
+def get_trained_data(frame, face):
     obj = frame_formating(frame, face)
-    cv2.imshow('data', obj)
     data = []
     for i in range(obj.shape[0]):
         data.append([])
         for j in range(obj.shape[1]):
             data[i].append(obj[i][j])
-    return np.expand_dims(data, 0), 1
+    return data
