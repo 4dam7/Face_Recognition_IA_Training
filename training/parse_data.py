e1a50c77075f3b1b62b46f0427527a3eeb9ac272
Adam Cohen <acohen@localhost.localdomain>
2018-07-19 12:06:05 +0200

    data parsing

--- training/parse_data.py
+++ training/parse_data.py
@@ -1,25 +1,30 @@
 import numpy as np
 import os
 
-def get_data(path):
+def get_data():
+    path = "../profils/"
     data = []
-    label =
-    a = 0
-    for f in os.listdir(path):
-        data.append([])
-        content = open(path + f, "r").read().splitlines()    
+    label = []
+    prof_id = -1
+    profils = []
+    a = -1
+    img = 28
+    n = 0
+    for f in os.listdir(path):        
+        content = open(path + f, "r").read().splitlines()
+        prof_id += 1
+        profils.append(f[:-3])
         for i in range(len(content)):
+            if img == 28:
+                data.append([])
+                img = 0
+                n = 0
+                a += 1
+                label.append(prof_id)       
             data[a].append([])
-            line = content[i].split()
+            line = content[i].split()    
             for j in range(len(line)):
-                data[a][i].append(int(line[j]))
-        a += 1    
-    return data, label
-
-data label = get_data("../profils/")
-
-print(data)
-            
-              
-
-    
+                data[a][n].append(int(line[j]))
+            img += 1
+            n += 1
+    return data, label, profils
