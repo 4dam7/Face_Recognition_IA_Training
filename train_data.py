fd3a433bb338a12e25c8692f77fe0a4612242249
Adam Cohen <acohen@localhost.localdomain>
2018-07-19 10:23:44 +0200

    train data

--- train_data.py
+++ train_data.py
@@ -6,28 +6,18 @@ import cv2
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('/home/acohen/Projet_Perso/openCV/haarcascade_xml/haarcascade_frontalface_default.xml')
 
-def frame_formating(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face = face_cascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
-    a = 0
+def frame_formating(frame, face):
     for (x, y, h, w) in face:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        crop_img = gray[y+1:y+1 + h, x+1:x+1 + w]
-        crop_img = cv2.resize(crop_img, (28, 28))
-        break        
-        a = 1
-    if a == 0:
-        return gray, 0
-    return crop_img, 1
+        crop_img = frame[y+1:y+1 + h, x+1:x+1 + w]
+        crop_img = cv2.resize(crop_img, (28, 28))        
+    return crop_img
 
-def get_train_data(frame):
-    obj, err = frame_formating(frame)
-    if err == 0:
-        return obj, 0
+def get_train_data(frame, face):
+    obj = frame_formating(frame, face)
     cv2.imshow('data', obj)
     data = []
     for i in range(obj.shape[0]):
         data.append([])
         for j in range(obj.shape[1]):
-            data[i].append(obj[i][j]) 
+            data[i].append(obj[i][j])
     return np.expand_dims(data, 0), 1
